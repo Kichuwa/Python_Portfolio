249e3090499eceb96df948fd0908bda6dbec082a
Darren Soulier <56926037+Kichuwa@users.noreply.github.com>
2024-04-07 22:14:27 -0500

    Created baseline look for multi-column

--- home.py
+++ home.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
+
+st.title("Welcome to my Portfolio!")
 
 col1, col2 = st.columns(2)
 
@@ -16,3 +19,24 @@ with col1:
 with col2:
     st.title("Darren Soulier")
     st.write(about_me_text)
+
+st.text("Below are some project I have built. Feel free to contact me!")
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+data_files = pandas.read_csv("data.csv", sep=";")
+
+
+with col3:
+    for index, row in data_files[:10].iterrows():
+        st.header(row["title"])
+        st.image(f'images/{row["image"]}')
+        st.write(row["description"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in data_files[10:].iterrows():
+        st.header(row["title"])
+        st.image(f'images/{row["image"]}')
+        st.write(row["description"])
+        st.write(f"[Source Code]({row['url']})")
