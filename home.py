9a7406923045318966af678655b44c686bc90fe9
Darren Soulier <56926037+Kichuwa@users.noreply.github.com>
2024-04-07 20:25:58 -0500

    Created columns for website layout.

--- home.py
+++ home.py
@@ -1,4 +1,18 @@
 import streamlit as st
 
-st.title("Welcome to my page")
+st.set_page_config(layout="wide")
 
+col1, col2 = st.columns(2)
+
+about_me_text = """
+I am Darren Soulier, a programmer who love to create and learn new things.
+I have worked with various tech stacks and have been able to grow my skill
+sets beyond exclusively development.
+"""
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Darren Soulier")
+    st.write(about_me_text)
